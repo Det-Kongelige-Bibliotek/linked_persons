fd8583f55918839ef0ced595156a9a3edca783ab
Murat Seyhan <muratseyhan5@gmail.com>
2017-05-08 15:21:29 +0200

    Added a SPARQL CONSTRUCT query to extract relevant data.

--- lib/lp/mixins/aggregatable.rb
+++ lib/lp/mixins/aggregatable.rb
@@ -45,13 +45,9 @@ module LP::Aggregatable
           graph_name: subject_uri)
       end
 
-    end 
+      extract_from_resource(resource)
 
-    # TODO: Retrieve the relevant information via SPARQL queries. 
-    #   solutions = SPARQL.execute("SELECT * WHERE { ?s ?p ?o }", resource.graph).
-    #   solutions.each do |sol|
-    #     graph << RDF::Statement(sol.s, sol.p, sol.o, graph_name: subject_uri) 
-    #   end
+    end 
 
     @aggregated = true
   end
@@ -62,6 +58,82 @@ module LP::Aggregatable
     end
   end
 
+  def extract_from_resource(resource)
+
+    # query = "SELECT * WHERE {?s ?p ?o}"
+    query =  %(
+      PREFIX wdata: <https://www.wikidata.org/wiki/Special:EntityData/>
+      PREFIX wd: <http://www.wikidata.org/entity/> 
+      PREFIX wdt: <http://www.wikidata.org/prop/direct/> 
+      PREFIX schema: <http://schema.org/> 
+      PREFIX skos: <http://www.w3.org/2004/02/skos/core#> 
+      PREFIX foaf:    <http://xmlns.com/foaf/0.1/>
+
+      CONSTRUCT { 
+        <#{subject_uri}> a ?type.
+        <#{subject_uri}> schema:name ?name.
+        <#{subject_uri}> wdt:P18 ?image.
+      }
+      WHERE {
+        <#{resource.subject_uri}> a ?type.
+        <#{resource.subject_uri}> schema:name ?name.
+        <#{resource.subject_uri}> schema:description ?description.
+        <#{resource.subject_uri}> wdt:P18 ?image.
+        <#{resource.subject_uri}> wdt:P19 ?placeOfBirth.
+        <#{resource.subject_uri}> wdt:P20 ?placeOfDeath.
+        <#{resource.subject_uri}> wdt:P21 ?gender.
+        <#{resource.subject_uri}> wdt:P27 ?countryOfCitizenship.
+        <#{resource.subject_uri}> wdt:P569 ?dateOfBirth.
+        <#{resource.subject_uri}> wdt:P570 ?dateOfDeath.
+        <#{resource.subject_uri}> wdt:106 ?occupation.
+        <#{resource.subject_uri}> skos:altLabel ?asKnownAs.
+        <#{resource.subject_uri}> wdt:P742 ?pseudonym.
+        <#{resource.subject_uri}> wdt:P103 ?nativeLanguage.
+        <#{resource.subject_uri}> wdt:P1412 ?usedLanguage.
+
+        ?placeOfBirth schema:name ?birthPlaceName.
+        ?placeOfDeath schema:name ?deathPlaceName.
+        ?gender schema:name ?genderName.
+        ?countryOfCitizenship schema:name ?countryOfCitizenshipName.
+        ?occupation schema:name ?occupationName.
+        ?nativeLanguage schema:name ?nativeLanguageName.
+        ?usedLanguage schema:name ?usedLanguageName.
+
+        FILTER ( lang(?name) = "en" 
+              || lang(?name) = "da" 
+              || lang(?name) = "")
+        FILTER ( lang(?birthPlaceName) = "en" 
+              || lang(?birthPlaceName) = "da" 
+              || lang(?birthPlaceName) = "")
+        FILTER ( lang(?deathPlaceName) = "en" 
+              || lang(?deathPlaceName) = "da" 
+              || lang(?deathPlaceName) = "")
+        FILTER ( lang(?genderName) = "en" 
+              || lang(?genderName) = "da" 
+              || lang(?genderName) = "")
+        FILTER ( lang(?countryOfCitizenshipName) = "en" 
+              || lang(?countryOfCitizenshipName) = "da" 
+              || lang(?countryOfCitizenshipName) = "")
+        FILTER ( lang(?occupationName) = "en" 
+              || lang(?occupationName) = "da" 
+              || lang(?occupationName) = "")
+        FILTER ( lang(?nativeLanguageName) = "en" 
+              || lang(?nativeLanguageName) = "da" 
+              || lang(?nativeLanguageName) = "")
+        FILTER ( lang(?usedLanguageName) = "en" 
+              || lang(?usedLanguageName) = "da" 
+              || lang(?usedLanguageName) = "")
+        }
+    )
+    statements = SPARQL.execute(query, resource.graph)
+    statements.each do |statement|
+ 
+      statement.graph_name = subject_uri
+      graph << statement
+ 
+    end
+
+  end
 
 
 end
