92927a410344be652ab6eae855d1219b7fb47970
Murat Seyhan <muratseyhan5@gmail.com>
2017-07-07 14:24:00 +0200

    Redis configuration now checks for the presence of the environment variable.

--- lib/lp/redis.rb
+++ lib/lp/redis.rb
@@ -1,9 +1,11 @@
 require 'redis'
 
 module LP
-  redis_path_array = ENV['REDIS_PORT'].split(':')
-  REDIS_PORT = redis_path_array.pop.to_i.freeze
-  REDIS_HOST = redis_path_array[1].split('//').last.freeze
+  unless ENV['REDIS_PORT'].nil?
+    redis_path_array = ENV['REDIS_PORT'].split(':')
+    REDIS_PORT = redis_path_array.pop.to_i.freeze
+    REDIS_HOST = redis_path_array[1].split('//').last.freeze
 
-  REDIS = Redis.new(host: REDIS_HOST, port: REDIS_PORT)
+    REDIS = Redis.new(host: REDIS_HOST, port: REDIS_PORT)
+  end
 end
